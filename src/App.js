d79f39c6f00f5f88b25ee6daf00bbd797ce4ce84
tvbbvv <102571551+tvbbvv@users.noreply.github.com>
2022-03-29 11:29:27 +0700

    Update App.js
    Update code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
 
 function App() {
   const address = useAddress();
-  const connectWithMetamask = useMetamask();
+  const connectWithMetamask = useMetamask(0x7EF0BCceCBB9c4c3307Acede6F86E52779C654bc);
   const disconnectWallet = useDisconnect();
   return (
     <div>
